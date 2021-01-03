70a107b6372cebf982b8d708811a66706a336358
Jennifer Luk <jennifer.luk21@ycdsbk12.ca>
2021-01-03 22:47:14 +0000

    added comments and corrected fare variable

--- src/UberSimulation/Booking.java
+++ src/UberSimulation/Booking.java
@@ -15,7 +15,20 @@ public class Booking {
   private String driverrating; 
   private double fare;
 
-  public Booking(String username, int duration, int distance, int passengernum, String usercarChoice, double Fare, String drivername, String driverphonenum, String driverrating) {
+  /** 
+  * Booking constructor. Creates an instance of the object, Booking. 
+  *
+  * @param username - a string that displays the name of the user
+  * @param duration - an integer that displays the duration of the ride 
+  * @param distance - an integer that displays the distance of the ride 
+  * @param passengernum - an integer that displays the number of passengers 
+  * @param usercarChoice - a string that displays the user's car choice
+   * @param fare - a double that displays the cost of the ride
+   * @param drivername - a string that displays the name of the driver 
+   * @param driverphonenum - a string that displays the phone number of the driver 
+   * @param driverrating - a string that displays the driver's rating 
+  */
+  public Booking(String username, int duration, int distance, int passengernum, String usercarChoice, double fare, String drivername, String driverphonenum, String driverrating) {
     this.username = username; 
     this.duration = duration; 
     this.distance = distance; 
@@ -27,7 +40,19 @@ public class Booking {
     this.driverrating = driverrating; 
   }
 
-  // print out booking 
+  /** 
+  * toString method that returns the booking information 
+  * 
+  * @return this.username - a string that displays the name of the user
+  * @return this.duration - an integer that displays the duration of the ride 
+  * @return this.distance - an integer that displays the distance of the ride 
+  * @return this.passengernum - an integer that displays the number of passengers 
+  * @return this.usercarChoice - a string that displays the user's car choice
+  * @return this.fare - a double that displays the cost of the ride
+  * @return this.drivername - a string that displays the name of the driver 
+  * @return this.driverphonenum - a string that displays the phone number of the driver 
+  * @return this.driverrating - a string that displays the driver's rating  
+  */ 
   public String toString() {
     return "\nHere is your booking: \nName: " + this.username + "\nRide Duration: " + this.duration + "\nRide Distance: " + this.distance + "\n# of Passengers: " +this.passengernum + "\nFare: " + this.fare + "\nCar Choice: " + this.usercarChoice + "\nDriver: " + this.drivername + "\nDriver Phone #: " + this.driverphonenum + "\nDriver Rating: " + this.driverrating;
   }
