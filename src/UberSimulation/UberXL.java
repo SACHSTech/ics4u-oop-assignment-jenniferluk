062c14c7e752b06774746f55ccbba76db02f3fa6
Jennifer Luk <jennifer.luk21@ycdsbk12.ca>
2021-01-03 23:34:07 +0000

    added comments to UberXL class

--- src/UberSimulation/UberXL.java
+++ src/UberSimulation/UberXL.java
@@ -10,17 +10,36 @@ public class UberXL extends Car {
   private int duration; 
   private int distance;
 
+  /** 
+  * UberXL constructor. Creates an instance of the object, uberblacksuv. 
+  *
+  * @param brand - string of car's brand called from the super constructor. 
+  * @param model - string of car's model called from the super constructor. 
+  * @param driver - string of driver's name called from the super constructor. 
+  * @param duration - integer of the ride's duration. 
+  * @param distance - integer of the ride's distance. 
+  */
   public UberXL(String brand, String model, String driver, int duration, int distance){
     super(brand, model,driver); 
     this.duration = duration;
     this.distance = distance;
   } 
 
+  /** 
+  * UberXL constructor that calls the above constructor and passes in the first three parameters. 
+  *
+  * @param duration - integer of the ride's duration. 
+  * @param distance - integer of the ride's distance. 
+  */
   public UberXL(int duration, int distance){
      this("Toyota","Hylander", "Amy", duration, distance);
   }
 
-  // get fare price
+  /** 
+  * Gettor method that returns the fare price. 
+  *
+  * @return the fare cost for UberXL.
+  */
   public double getFare(){
     return 5.00 + (duration*0.35) + (distance*1.55);
   }
